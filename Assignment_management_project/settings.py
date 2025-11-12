94fe7643b7923951abab4580a077f11183d1329a
vishnuprasad003 <appuvishnuprasad61@gmail.com>
2025-11-12 13:26:01 +0530

    second commit

--- Assignment_management_project/settings.py
+++ Assignment_management_project/settings.py
@@ -76,11 +76,21 @@ WSGI_APPLICATION = 'Assignment_management_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "Assignment_management_DB",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
